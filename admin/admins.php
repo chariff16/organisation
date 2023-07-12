56a92b4758941b8e6a60a155e8b447bcaf840062
chariff16 <chariff16@gmail.com>
2023-07-12 02:24:03 +0100

    adding posts UI

--- admin/admins.php
+++ admin/admins.php
@@ -291,18 +291,6 @@
                     </div>
                   </form>
                 </div>
-                <div class="modal-footer">
-                  <button
-                    type="button"
-                    class="btn btn-secondary"
-                    data-bs-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="button" class="btn btn-primary">
-                    Save changes
-                  </button>
-                </div>
               </div>
             </div>
           </div>
