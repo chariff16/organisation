bb8df2e479d88b7720518e4179bf6270388182f7
chariff16 <chariff16@gmail.com>
2023-07-13 17:14:00 +0100

    all donners UI done

--- admin/donners.php
+++ admin/donners.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/bootstrap.css" />
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
     <link rel="stylesheet" href="../css/admin_dashbord.css" />
     <link
       rel="stylesheet"
@@ -140,12 +141,266 @@
         <div>
           <h3 class="text-primary text-center mt-3">قائمة المحسنين</h3>
         </div>
+        <div class="mt-3 mx-5">
+        <button
+            type="button"
+            class="btn btn-outline-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addDonnerModal"
+          >
+            إضافة محسن
+        </button>
         <div class="mt-4">
-          <p class="text-center">here the view of the dashbord</p>
+          <table class="table table-hover" id="table">
+            <thead>
+              <tr>
+                <th scope="col">الاسم</th>
+                <th class="col">اللقب</th>
+                <th class="col">المبلغ</th>
+                <th class="col-1">
+                  <p class="d-none d-md-block mb-0">تعديل/معاينة/حذف</p>
+                  <p class="d-md-none mb-0">فعل</p>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                require("../dbcon.php");
+                $sql = "SELECT * FROM `donner`";
+                $run = mysqli_query($con, $sql);
+                while($row = mysqli_fetch_assoc($run)){
+              ?>
+              <tr>
+                <td class="align-middle"><?php echo $row['fname'] ?></td>
+                <td class="align-middle"><?php echo $row['lname'] ?></td>
+                <td class="align-middle"><?php echo $row['funds'] ?> دينار</td>
+                <td class="d-md-flex d-sm-inline-block justify-content-around">
+                  <button
+                    type="button"
+                    class="btn btn-outline-primary btn-sm editBtn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#editDonnerModal"
+                    value="<?php echo $row['id'] ?>"
+                  >
+                    <i class="far fa-edit"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-outline-success btn-sm viewBtn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#viewDonnerModal"
+                    value="<?php echo $row['id'] ?>"
+                  >
+                    <i class="fas fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-outline-danger btn-sm deleteBtn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#deleteDonnerModal"
+                    value="<?php echo $row['id'] ?>"
+                  >
+                  <i class="fas fa-trash-alt"></i>
+                  </button>
+                </td>
+              </tr>
+              <?php
+                }
+              ?>
+            </tbody>
+          </table>
         </div>
       </main>
+      <div class="mOdels">
+          <div class="modal fade" id="addDonnerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    إضافة محسن
+                  </h1>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <form id="addDonner">
+                    <label class="form-label donnerFnameLable">اسم المحسن</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control donnerFname"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <label class="form-label donnerLnameLable">لقب المحسن</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control donnerLname"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <label class="form-label donerPhoneLable">هاتف المحسن</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control donnerPhone"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <label class="form-label donnerFundsLable">مبلغ التبرع</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control donnerFunds"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <div class="modal-footer">
+                      <input
+                        type="submit"
+                        name="add"
+                        class="btn btn-primary"
+                        value="إضافة محسن"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal fade" id="editDonnerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    تعديل معلومات محسن
+                  </h1>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <form id="editPost">
+                    <input type="text" class="d-none editDonnerId" name="editDonnerId">
+                    <label class="form-label editDonnerFnameLable">اسم المحسن</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control editDonnerFname"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <label class="form-label editDonnerLnameLable">لقب المحسن</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control editDonnerLname"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <label class="form-label editDonerPhoneLable">هاتف المحسن</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control editDonnerPhone"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <label class="form-label editDonnerFundsLable">مبلغ التبرع</label>
+                    <input
+                      type="text"
+                      name="postTitel"
+                      class="form-control editDonnerFunds"
+                      aria-labelledby="passwordHelpBlock"
+                    />
+                    <div class="modal-footer">
+                      <input
+                        type="submit"
+                        name="add"
+                        class="btn btn-primary"
+                        value="تحديث المعلومات"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal fade" id="viewDonnerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    معلومات المحسن
+                  </h1>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <div class="d-flex">
+                    <p class="col-2">الإسم</p>
+                    <p class="col fw-bold viewPostTitel">محمد</p>
+                  </div>
+                  <div class="d-flex">
+                    <p class="col-2">اللقب</p>
+                    <p class="col fw-bold viewPostTitel">محمد</p>
+                  </div>
+                  <div class="d-flex">
+                    <p class="col-2">الهاتف</p>
+                    <p class="col fw-bold viewPostTitel">محمد</p>
+                  </div>
+                  <div class="d-flex">
+                    <p class="col-2">المبلغ المتبرع بيه</p>
+                    <p class="col fw-bold viewPostTitel align-self-center">محمد</p>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    إغلاق
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal fade" id="deleteDonnerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    حذف محسن
+                  </h1>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <input type="text" class="d-none deleteId" >
+                  <p>هل أنت متأكد من حذف هذا المحسن</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" id="deletePost">
+                    حذف
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
     </section>
     <script src="../js/bootstrap.bundle.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
   </body>
 </html>
