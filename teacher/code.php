93cb5c87ece3ff6fc4ff5cfe8eddc8847c7c59e8
chariff16 <chariff16@gmail.com>
2023-08-06 20:18:26 +0100

    creating post page with UI & Functions

--- teacher/code.php
+++ teacher/code.php
@@ -123,3 +123,25 @@ if (isset($_GET['deleteExam'])) {
         return ;
     }
 };
+if (isset($_GET['view_post_id'])) {
+    require('../dbcon.php');
+    $id = mysqli_real_escape_string($con, $_GET['view_post_id']);
+    $sql = "SELECT * FROM `postes` WHERE id = '$id'";
+    $run = mysqli_query($con, $sql);
+    if ($row = mysqli_fetch_assoc($run)) {
+        $res = [
+            'code' => 200,
+            'message' => 'تمت إضافة عضو',
+            'data' => $row
+        ];
+        echo json_encode($res);
+        return ;
+    }else {
+        $res = [
+            'code' => 404,
+            'message' => 'يرجى الإتصال بمطور الموقع'
+        ];
+        echo json_encode($res);
+        return ;
+    }
+};
